3c579b4561bd91b98983f6e81fc0265b6dae0d94
ikotsov <i.kotsovilis@gmail.com>
2024-02-28 15:09:25 +0200

    refactor: to modify grey colours

--- app/components/list-item.tsx
+++ app/components/list-item.tsx
@@ -43,7 +43,7 @@ const styles = StyleSheet.create({
     fontWeight: "500",
   },
   subtitle: {
-    color: baseColors.grey,
+    color: baseColors.grey.medium,
   },
 });
 
--- app/constants/colors.ts
+++ app/constants/colors.ts
@@ -6,7 +6,10 @@ export const baseColors = {
   secondary: "#4ecdc4",
   black: "#000",
   white: "#fff",
-  grey: "#6e6969",
+  grey: {
+    medium: "#6e6969",
+    light: "#f8f4f4",
+  },
 };
 
 export default {
