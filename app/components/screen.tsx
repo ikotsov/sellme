c24c115689b46ed536051d20a9d8ef6b32d523ff
gkotsovilis <i.kotsovilis@gmail.com>
2024-03-08 18:28:33 +0200

    fix: imports

--- app/components/screen.tsx
+++ app/components/screen.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ColorValue, StyleProp, View, ViewStyle } from "react-native";
+import { StyleProp, View, ViewStyle } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import { baseColors } from "@constants/colors";
